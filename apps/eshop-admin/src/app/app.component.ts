e27168f0a2450f2b0e4fc8c1a5f70595a0cbd855
PHOEM Phanith <61651733+phoemphanith@users.noreply.github.com>
2022-11-19 11:57:43 +0700

    config: initial style

--- apps/eshop-admin/src/app/app.component.ts
+++ apps/eshop-admin/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'eshop-root',
+  selector: 'eshop-admin-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'eshop-admin';
