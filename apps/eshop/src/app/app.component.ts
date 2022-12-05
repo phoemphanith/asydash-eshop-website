894ffe041e7df019143cca99438e17f7fa72dbb4
PHOEM Phanith <61651733+phoemphanith@users.noreply.github.com>
2022-12-05 16:32:21 +0700

    fix: error app.component.scss (echop)

--- apps/eshop/src/app/app.component.ts
+++ apps/eshop/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'eshop-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'eshop';
